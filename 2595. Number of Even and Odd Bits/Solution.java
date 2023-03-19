5a0e1da522e7a5412048e476aec93a7c93d0bbfc
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-03-19 18:43:27 +0530

    fixed grammer error

--- 2595. Number of Even and Odd Bits/Solution.java
+++ 2595. Number of Even and Odd Bits/Solution.java
@@ -2,10 +2,10 @@
  * 2595. Number of Even and Odd Bits
  * 
  * approach-
- * we will first find binary equivalent of n and store it in an array.
+ * we will first find the binary equivalent of n and store it in an array.
  * we find binary equivalent by storing modulo of n with 2 and decreasing n by dividing by 2.
- * Then we will reverse the array to get binary equivalent.
- * we will traverse the array and check for 1, and increase even and odd count based on index.
+ * Then we will reverse the array to get the binary equivalent.
+ * we will traverse the array and check for 1, and increase even and odd counts based on the index.
  * 
  * Time - O(n)
  * Space - O(n)
