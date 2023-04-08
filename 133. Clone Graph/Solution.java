acec50c1bb3ff1a8bbd6ca63857b008214269170
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-04-08 17:43:47 +0530

    added DFS approach

--- 133. Clone Graph/Solution.java
+++ 133. Clone Graph/Solution.java
@@ -18,6 +18,8 @@
  */
 
 /*
+ * BFS Code
+ * 
  * // Definition for a Node.
  * class Node {
  * public int val;
@@ -60,6 +62,47 @@ class Solution {
                 }
             }
         }
+        return map.get(node);
+    }
+}
+
+/*
+ * DFS Code
+ * 
+ * // Definition for a Node.
+ * class Node {
+ * public int val;
+ * public List<Node> neighbors;
+ * public Node() {
+ * val = 0;
+ * neighbors = new ArrayList<Node>();
+ * }
+ * public Node(int _val) {
+ * val = _val;
+ * neighbors = new ArrayList<Node>();
+ * }
+ * public Node(int _val, ArrayList<Node> _neighbors) {
+ * val = _val;
+ * neighbors = _neighbors;
+ * }
+ * }
+ */
+
+class Solution {
+    HashMap<Node, Node> map = new HashMap();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null)
+            return null;
+        if (map.containsKey(node))
+            return map.get(node);
+
+        map.put(node, new Node(node.val));
+        for (Node neigh : node.neighbors) {
+            map.get(node).neighbors.add(cloneGraph(neigh));
+        }
+
         return map.get(node);
     }
 }
