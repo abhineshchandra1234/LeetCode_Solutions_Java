d85ca4fb06141f4ed4d89bb07630850d7fedca08
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-04-11 19:56:38 +0530

    2390. Removing Stars From a String

---
+++ 2390. Removing Stars From a String/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * 2390. Removing Stars From a String
+ * 
+ * Intuition
+ * We will use StringBuilder to solve this
+ * If we encounter '*' we will delete last char of StringBuilder else will add
+ * char to StringBuilder
+ * Finally return the result by converting it to string
+ * 
+ * Approach
+ * Initialize the StringBuilder
+ * Traverse the chars of string, if the char is "*" then delete last char of
+ * StringBuilder else add the char to the StringBuilder
+ * 
+ * Complexity
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+class Solution {
+    public String removeStars(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '*')
+                sb.deleteCharAt(sb.length() - 1);
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+}
