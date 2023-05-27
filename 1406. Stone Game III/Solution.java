859e765231149aa4ec6de9fa5ce50fb8d07018c6
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-05-27 19:05:39 +0530

    added second soln

--- 1406. Stone Game III/Solution.java
+++ 1406. Stone Game III/Solution.java
@@ -47,3 +47,28 @@ class Solution {
         return "Tie";
     }
 }
+
+/**
+ * Solution 2: O(1) Space
+ * We don't need O(N) space.
+ * We only need last 3 results: dp[i+1], dp[i+2] and dp[i+3].
+ * Time O(N), space O(1)
+ */
+
+class Solution {
+    public String stoneGameIII(int[] A) {
+        int n = A.length, dp[] = new int[4];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i % 4] = Integer.MIN_VALUE;
+            for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+                take += A[i + k];
+                dp[i % 4] = Math.max(dp[i % 4], take - dp[(i + k + 1) % 4]);
+            }
+        }
+        if (dp[0] > 0)
+            return "Alice";
+        if (dp[0] < 0)
+            return "Bob";
+        return "Tie";
+    }
+}
