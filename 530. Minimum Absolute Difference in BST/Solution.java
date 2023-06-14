67a0c4c2a6f914a7416b71e2c93fc1b6a533c257
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-06-15 00:32:00 +0530

    fixed bug

--- 530. Minimum Absolute Difference in BST/Solution.java
+++ 530. Minimum Absolute Difference in BST/Solution.java
@@ -1,21 +1,22 @@
 /**
  * 530. Minimum Absolute Difference in BST
  * 
+ * 
  * Intuition
- * 
- * credits shawngao
- * we can solve this problem using DFS (inorder traversal).
- * When we do DFS of bst it returns an sorted list.
- * to get min absolute diff between any two nodes. We need diff of consecutive
- * nodes.
- * 
- * Complexity
- * 
- * Time complexity: O(n)
- * 
- * Space complexity: O(n)
- * 
+
+    credits shawngao
+    we can solve this problem using DFS (inorder traversal).
+    When we do inorder traversal of bst it returns an sorted list.
+    to get min absolute diff between any two nodes. We need diff of consecutive nodes.
+
+Complexity
+
+    Time complexity: O(n)
+
+    Space complexity: O(n)
+ 
  */
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -25,9 +26,17 @@
  * TreeNode() {}
  * TreeNode(int val) { this.val = val; }
  * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
+ * 
+ * 
+ * import java.util.Stack;
+ * 
+ * import javax.swing.tree.TreeNode;
+ * 
+ * 
+ * import java.util.Stack;
+ * 
+ * import javax.swing.tree.TreeNode;
+ * 
  * }
  * }
  */
