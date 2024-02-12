e03480f87a430a08c6cf17e9850d2576a3d4e46c
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-02-12 23:47:48 +0530

    211. Design Add and Search Words Data Structure

--- leetcode solutions/211. Design Add and Search Words Data Structure/Solution.java
+++ leetcode solutions/211. Design Add and Search Words Data Structure/Solution.java
@@ -1,70 +1,103 @@
 /**
  * 211. Design Add and Search Words Data Structure
  * 
- * approach-
- * It has similar logic as 208. Implement Trie (Prefix Tree)
- * search method will be modified a little
+ * Intuition
  * 
- * search-
- * when we encounter dot char we will use recursion to solve this.
- * we will check all 26 chars if it leads to a word, we will use pos to traverse word
- * eg - search - b.t, words - bar, bmt
- * In the above example you can see after 'b' there is a dot.
- * first we search through 'a' we get false, we will not return this value
- * we will keep searching through 'm' we get true, we will return this value
- * And suppose if all 26 chars doesnt return true then finally we will return false.
- * eg - search - b., words - b
+ * It has similar implementation as of
+ * 208. Implement Trie (Prefix Tree)
+ * We will just tweak the search function to accomodate the dot char
+ * we will search all 26 options for every non-null node
+ * When we reach end of string instead of returning false, we will return isEnd
+ * we do this as we are not checking isEnd in case of dot char as we do with
+ * other chars
+ * if all nodes are null for dot character then return false
+ * means there are no more child nodes
+ * eg - we have "ba" in database and we are searching "b.."
  * 
- * Time - O(n*n)
- * Space - O(n*n)
+ * Approach
+ * Complexity
+ * 
+ * Time complexity:
+ * insert - O(n)
+ * search - O(26^n), for every dot character, there will be 26
+ * suitable options
+ * startsWith - O(n)
+ * 
+ * Space complexity:
+ * insert - O(n)
+ * search - O(1)
+ * startsWith - O(1)
+ * 
  */
 class WordDictionary {
+    Node root;
 
-    class TrieNode {
-        TrieNode[] child = new TrieNode[26];
-        boolean isWord = false;
-    }
-    TrieNode root;
     public WordDictionary() {
-        root = new TrieNode();
+        root = new Node();
     }
-    
+
     public void addWord(String word) {
-        TrieNode temp = root;
-        for(char c:word.toCharArray()) {
-            int i = c-'a';
-            if(temp.child[i]==null)
-                temp.child[i] = new TrieNode();
-            temp = temp.child[i];
-        }
-        temp.isWord = true;
+        root.insert(word, 0);
     }
 
     public boolean search(String word) {
-        TrieNode temp = root;
-        return helper(temp,0,word);
+        return root.search(word, 0);
     }
 
-    public boolean helper(TrieNode temp,int pos,String word) {
-        for(int i=pos;i<word.length();i++) {
-            if(word.charAt(i)=='.') {
-                for(int j=0;j<26;j++) {
-                    if(temp.child[j]!=null) {
-                        if(helper(temp.child[j],i+1,word))
-                            return true;
-                    }
-                }
+    class Node {
+        Node[] nodes;
+        boolean isEnd;
+
+        Node() {
+            nodes = new Node[26];
+        }
+
+        private void insert(String word, int idx) {
+            int n = word.length();
+            // to avoid null ptr exception for string
+            // no more chars to insert
+            if (idx >= n)
+                return;
+            int i = word.charAt(idx) - 'a';
+            if (nodes[i] == null)
+                nodes[i] = new Node();
+            if (idx == n - 1)
+                nodes[i].isEnd = true;
+            nodes[i].insert(word, idx + 1);
+        }
+
+        private boolean search(String word, int idx) {
+            int n = word.length();
+            // all chars searched
+            if (idx >= n)
+                return isEnd;
+            char c = word.charAt(idx);
+            if (c == '.') {
+                for (Node node : nodes)
+                    if (node != null && node.search(word, idx + 1))
+                        return true;
                 return false;
             }
-            else {
-                int in = word.charAt(i)-'a';
-                if(temp.child[in]!=null)
-                    temp = temp.child[in];
-                else
-                    return false;
-            }
+            Node node = nodes[word.charAt(idx) - 'a'];
+            if (node == null)
+                return false;
+            if (idx == n - 1 && node.isEnd)
+                return true;
+            return node.search(word, idx + 1);
         }
-        return temp.isWord;
+
+        private boolean startsWith(String prefix, int idx) {
+            int n = prefix.length();
+            // all chars searched
+            if (idx >= n)
+                return false;
+            Node node = nodes[prefix.charAt(idx) - 'a'];
+            if (node == null)
+                return false;
+            if (idx == n - 1)
+                return true;
+            return node.startsWith(prefix, idx + 1);
+        }
     }
 }
 
