29022f935c5ebe9c7624f406618a8dbda04fa5e2
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-03-07 07:26:23 +0530

    876. Middle of the Linked List

---
+++ leetcode solutions/876. Middle of the Linked List/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * 876. Middle of the Linked List
+ * 
+ * Intuition
+ * 
+ * Same as other problems in linked list we will solve this using 2 ptrs
+ * fast will increase by 2 steps and slow by 1 step. So when fast will point to
+ * end of list, slow will point to middle of the list.
+ * 
+ * Approach
+ * Complexity
+ * 
+ * Time complexity: O(n)
+ * 
+ * Space complexity: O(1)
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
