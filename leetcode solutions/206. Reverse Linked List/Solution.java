0b35d063555783108259626a6526f5dce69264a7
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-03-21 23:48:51 +0530

    206. Reverse Linked List

---
+++ leetcode solutions/206. Reverse Linked List/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * 206. Reverse Linked List
+ * 
+ * Intuition
+ * 
+ * we will solve this problem using two ptrs
+ * Using first ptr (head) we will traverse the list and point curr node to
+ * previous node using second ptr (prev)
+ * 
+ * Approach
+ * Complexity
+ * 
+ * Time complexity: O(n)
+ * 
+ * Space complexity: O(1)
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
