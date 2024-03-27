5399d18d1c0ad3c57775258e0a9efb17ee1827b4
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-03-27 23:42:48 +0530

    713. Subarray Product Less Than K

---
+++ leetcode solutions/713. Subarray Product Less Than K/Solution.java
@@ -0,0 +1,39 @@
+/*
+ * 713. Subarray Product Less Than K
+ * 
+ * Intuition
+ * 
+ * we will solve this problem using two ptrs and sliding window
+ * right ptr will expand the window and contain total product
+ * left ptr will shrink the window till product becomes less than k
+ * No of subarrays for current window will be equal to its size ie right - left
+ * + 1 and will be added to res
+ * 
+ * Approach
+ * Complexity
+ * 
+ * Time complexity: O(n)
+ * 
+ * Space complexity: O(1)
+ * 
+ */
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+        if (k <= 1)
+            return 0;
+
+        int product = 1, total = 0, n = nums.length;
+
+        for (int left = 0, right = 0; right < n; right++) {
+            product *= nums[right];
+
+            while (product >= k)
+                product /= nums[left++];
+
+            total += right - left + 1;
+        }
+
+        return total;
+    }
+}
