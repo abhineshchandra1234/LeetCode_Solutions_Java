0679f937ae10ce93268e516e4d5cfeb607818318
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-05-04 19:27:45 +0530

    881. Boats to Save People

--- leetcode solutions/881. Boats to Save People/Solution.java
+++ leetcode solutions/881. Boats to Save People/Solution.java
@@ -2,43 +2,55 @@
  * 881. Boats to Save People
  * 
  * Intuition
- * We will try to accommodate the heaviest and thinnest person if that is not
- * possible we will accommodate only the heaviest person
+ * Target
+ * 
+ * minimum no of boats to carry all people
+ * 
+ * Given
+ * 
+ * weight of people will be less than or equal to limit
+ * 
+ * Solution
+ * 
+ * We will solve this problem using two-ptrs
+ * If it was just the count of boat we could have solved it by simple traversal
+ * According to question we need minimum boats, so we will accomodate heaviest
+ * and thinnest person, if that is not possible we will accomodate only heaviest
+ * person as it will be less than or equal to limit.
  * 
  * Approach
+ * 
  * sort the people array
- * use two ptrs approach, to accommodate the heaviest and thinnest person, if
- * is possible to update both ptrs
- * if the above is not possible, then accommodate only the heaviest person and
- * update the end
+ * use two ptrs approach, to accomodate the heaviest and thinnest person, if
+ * that is possible update both ptrs
+ * if above is not possible, then accomodate only heaviest person and update end
  * ptr
  * Increase boat count by 1 in each loop
- * If both ptr are equal we will need one boat, no matter if it is greater or
- * less
- * than the limit, BCS only one person is left.
+ * If both ptr are equal we will need one boat, no matter it is greater or less
+ * than limit, bcs only one person is left.
  * 
  * Complexity
+ * 
  * Time complexity: O(nlogn)
+ * 
  * Space complexity: O(1)
+ * 
  */
 
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
 
         Arrays.sort(people);
-        int start = 0;
-        int end = people.length - 1;
-        int minBoat = 0;
+        int start = 0, end = people.length - 1, count = 0;
 
         while (start <= end) {
             if (people[start] + people[end] <= limit) {
                 start++;
                 end--;
-            } else {
+            } else
                 end--;
-            }
-            minBoat++;
+            count++;
         }
-        return minBoat;
+        return count;
     }
 }
