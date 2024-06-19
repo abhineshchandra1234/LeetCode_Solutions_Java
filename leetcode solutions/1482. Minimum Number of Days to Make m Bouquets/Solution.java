e6afe6404c7929c77e9468529ac5941a5fb59c46
abhinesh chandra <abhineshchandra1234@gmail.com>
2024-06-19 20:18:27 +0530

    formatted solution

--- leetcode solutions/1482. Minimum Number of Days to Make m Bouquets/Solution.java
+++ leetcode solutions/1482. Minimum Number of Days to Make m Bouquets/Solution.java
@@ -2,30 +2,33 @@
  * 1482. Minimum Number of Days to Make m Bouquets
  * 
  * Intuition
-
-    we have been given an array of flowers bloom dates
-    we have also been given integer m and k
-    we need to return min days required to make m bouqets using k adjacent bloomed flowers
-    If it is not possible return -1.  
-    So how to get min days
-
-Approach
-
-    First we will check if we have enough flowers to form bouqets
-    We will apply binary search on max bloom days
-    For each day we will check if we can get suitable bouqets using a helper function
-    If we get a suitable res, we will try to find more optimized one by trying min days
-    since we need to find min days, used binary algo to find min res
-
-Complexity
-
-    Time complexity: O(nlogn)
-
-    Space complexity: O(1)
-
+ * 
+ * we have been given an array of flowers bloom dates
+ * we have also been given integer m and k
+ * we need to return min days required to make m bouqets using k adjacent
+ * bloomed flowers
+ * If it is not possible return -1.
+ * So how to get min days
+ * 
+ * Approach
+ * 
+ * First we will check if we have enough flowers to form bouqets
+ * We will apply binary search on max bloom days
+ * For each day we will check if we can get suitable bouqets using a helper
+ * function
+ * If we get a suitable res, we will try to find more optimized one by trying
+ * min days
+ * since we need to find min days, used binary algo to find min res
+ * 
+ * Complexity
+ * 
+ * Time complexity: O(nlogn)
+ * 
+ * Space complexity: O(1)
+ * 
  */
 
- class Solution {
+class Solution {
     public int minDays(int[] bloom, int m, int k) {
 
         if (bloom.length < (long) m * k)
