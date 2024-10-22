5aa09a4041332eebb680875a18db98f77079ad48
abhinesh chandra <abhineshchandra1234@gmail.com>
2024-10-22 18:24:07 +0530

    updated content

--- leetcode solutions/2583. Kth Largest Sum in a Binary Tree/2583_Kth_Largest_Sum_in_a_Binary_Tree.java
+++ leetcode solutions/2583. Kth Largest Sum in a Binary Tree/2583_Kth_Largest_Sum_in_a_Binary_Tree.java
@@ -1,56 +1,63 @@
-//2583. Kth Largest Sum in a Binary Tree
-/*
- * approach - 
- * we will solve this problem using min heap.
- * we will do level order traversal of the tree and store sum in heap.
- * to get kth largest sum we will keep heap size limited to k
- * At last if heap size is equal to k we will return the top element
- * else if heap size is less than k we will return -1
+/**
+ * 2583. Kth Largest Sum in a Binary Tree
  * 
- * Time - O(nlogn)
- * Space - O(n)
+ * Intuition
+ * we can solve this problem using BFS and min heap
+ * we require min heap, to check the no of labels
+ * min heap will store all the sums of the level
+ * Approach
+ * 
+ * 
+ * Complexity
+ * Time complexity: O(n)*log(k), traversing n nodes and k nodes adjusting in
+ * heap
+ * Space complexity: O(n), adding all nodes to queue
  */
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+
     public long kthLargestLevelSum(TreeNode root, int k) {
-        
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        
+
+        PriorityQueue<Long> pq = new PriorityQueue();
+
         Queue<TreeNode> q = new LinkedList();
+
         q.add(root);
-        while(!q.isEmpty()) {
+
+        while (!q.isEmpty()) {
+
             int size = q.size();
-            long sum  = 0;
-            while(size-->0) {
-                TreeNode node = q.poll();
-                sum += node.val;
-                if(node.left!=null)
-                    q.add(node.left);
-                if(node.right!=null)
-                    q.add(node.right);
+            long sum = 0;
+            while (size-- > 0) {
+                TreeNode curr = q.poll();
+                sum += curr.val;
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
             }
             pq.add(sum);
-            if(pq.size()>k)
+            if (pq.size() > k)
                 pq.poll();
         }
-        
-        if(pq.size()<k)
+
+        if (pq.size() < k)
             return -1;
-        else
-            return pq.poll();
+        return pq.peek();
     }
 }
