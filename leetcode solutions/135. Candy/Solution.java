bd3924a7fa6e032b95f18fbbaec11f2b2396c70d
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2025-12-29 18:49:22 +0530

    135. Candy

--- leetcode solutions/135. Candy/Solution.java
+++ leetcode solutions/135. Candy/Solution.java
@@ -43,3 +43,25 @@ class Solution {
         return sum;
     }
 }
+
+// kotlin
+
+class Solution {
+    fun candy(ratings: IntArray): Int {
+        
+        val n = ratings.size
+        val candies = IntArray(n) {1}
+
+        for(i in 1 until n) {
+            if(ratings[i]>ratings[i-1])
+                candies[i] = candies[i-1] + 1
+        }
+
+        for(i in n-2 downTo 0) {
+            if(ratings[i]>ratings[i+1])
+                candies[i] = Math.max(candies[i], (candies[i+1]+1))
+        }
+
+        return candies.sum()
+    }
+}
