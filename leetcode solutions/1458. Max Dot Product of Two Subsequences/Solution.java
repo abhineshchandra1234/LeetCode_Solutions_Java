4bc15a27b0099cbec378e8b0d76de82ed47f3c76
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2026-01-08 19:40:35 +0530

    1458. Max Dot Product of Two Subsequences

--- leetcode solutions/1458. Max Dot Product of Two Subsequences/Solution.java
+++ leetcode solutions/1458. Max Dot Product of Two Subsequences/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 1458. Max Dot Product of Two Subsequences
  * 
@@ -9,6 +11,8 @@
  * first -> pick both and move to next indices
  * second -> pick nums1 and skip nums2
  * third -> skip nums1 and pick nums2
+ * Use dynamic programming, define DP[i][j] as the maximum dot product
+ * of two subsequences starting in the position i of nums1 and position j of nums2.
  * 
  * Approach
  * 
@@ -69,5 +73,47 @@ class Solution {
         memo[i][j] = Math.max(use, Math.max(dp(i + 1, j, nums1, nums2),
                 dp(i, j + 1, nums1, nums2)));
         return memo[i][j];
+    }
+}
+
+// Alternative approach
+
+class Solution {
+    int[] nums1, nums2;
+    int[][] memo;
+    int n, m;
+    int neg = (int) -1e9;
+
+    public int maxDotProduct(int[] a, int[] b) {
+        nums1 = a;
+        nums2 = b;
+        n = a.length;
+        m = b.length;
+
+        memo = new int[n][m];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(memo[i], Integer.MIN_VALUE);
+        return dp(0, 0);
+    }
+
+    private int dp(int i, int j) {
+        if (i == n || j == m)
+            return neg;
+        if (memo[i][j] != Integer.MIN_VALUE)
+            return memo[i][j];
+
+        int take = nums1[i] * nums2[j];
+
+        int res = Math.max(
+                Math.max(
+                        take, // take and end here
+                        take + dp(i + 1, j + 1) // take both and continue
+                ),
+                Math.max(
+                        dp(i + 1, j), // skip nums1[i]
+                        dp(i, j + 1) // skip nums2[j]
+                ));
+
+        return memo[i][j] = res;
     }
 }
