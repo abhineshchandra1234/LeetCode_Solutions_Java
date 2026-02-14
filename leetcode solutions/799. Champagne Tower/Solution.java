c7ec9343e1b10f89ceee5aa55ad42253ec8cb26d
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2026-02-14 18:50:44 +0530

    799. Champagne Tower

--- leetcode solutions/799. Champagne Tower/Solution.java
+++ leetcode solutions/799. Champagne Tower/Solution.java
@@ -47,3 +47,36 @@ class Solution {
         return Math.min(1, prevRow.get(query_glass));
     }
 }
+
+class Solution {
+    double[][] t = new double[101][101];
+
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                t[i][j] = -1;
+            }
+        }
+
+        return Math.min(1.0, solve(poured, query_row, query_glass));
+    }
+
+    private double solve(int pour, int i, int j) {
+        if (i < 0 || j > i || j < 0)
+            return 0.0;
+        if (i == 0 && j == 0)
+            return t[i][j] = pour;
+        if (t[i][j] != -1)
+            return t[i][j];
+
+        double up_l = (solve(pour, i - 1, j - 1) - 1) / 2.0;
+        double up_r = (solve(pour, i - 1, j) - 1) / 2.0;
+
+        if (up_l < 0)
+            up_l = 0.0;
+        if (up_r < 0)
+            up_r = 0.0;
+
+        return t[i][j] = up_l + up_r;
+    }
+}
