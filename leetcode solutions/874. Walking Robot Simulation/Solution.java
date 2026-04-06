12474364541182a2dd1869ba94041516861b844e
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2026-04-06 18:43:39 +0530

    874. Walking Robot Simulation

--- leetcode solutions/874. Walking Robot Simulation/Solution.java
+++ leetcode solutions/874. Walking Robot Simulation/Solution.java
@@ -87,3 +87,45 @@ class Solution {
         return x + hashed * y;
     }
 }
+
+// Alternate code
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        HashSet<String> obsSet = new HashSet();
+        for (int[] obs : obstacles) {
+            String key = obs[0] + "_" + obs[1];
+            obsSet.add(key);
+        }
+
+        int x = 0;
+        int y = 0;
+        int maxD = 0;
+
+        int[] dir = { 0, 1 };
+
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i] == -2)
+                dir = new int[] { -dir[1], dir[0] };
+            else if (commands[i] == -1)
+                dir = new int[] { dir[1], -dir[0] };
+            else {
+                for (int step = 0; step < commands[i]; step++) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+
+                    String nextKey = newX + "_" + newY;
+
+                    if (obsSet.contains(nextKey))
+                        break;
+
+                    x = newX;
+                    y = newY;
+                }
+            }
+
+            maxD = Math.max(maxD, x * x + y * y);
+        }
+        return maxD;
+    }
+}
