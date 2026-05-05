0c6cdbdce3f834d4aefa809c8ca03d8aae4df99b
abhinesh chandra <abhineshchandra1234@gmail.com>
2026-05-05 12:59:31 +0530

    151. Reverse Words in a String

--- leetcode solutions/151. Reverse Words in a String/Solution.java
+++ leetcode solutions/151. Reverse Words in a String/Solution.java
@@ -12,6 +12,7 @@ Time complexity: O(n)
 Space complexity: O(n)
 */
 
+//kotlin
 class Solution {
     fun reverseWords(s: String): String {
         val words = s.split(" ").toMutableList()
@@ -36,5 +37,35 @@ class Solution {
             }
         }
         return res.toString()
+    }
+}
+
+//java
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int i = s.length() - 1;
+
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) == ' ')
+                i--;
+
+            if (i < 0)
+                break;
+
+            int j = i;
+
+            while (j >= 0 && s.charAt(j) != ' ')
+                j--;
+
+            if (result.length() > 0)
+                result.append(" ");
+
+            result.append(s.substring(j + 1, i + 1));
+
+            i = j;
+        }
+
+        return result.toString();
     }
 }
