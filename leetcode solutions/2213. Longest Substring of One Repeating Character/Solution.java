376a79252f621aa3a71c59e5de95a3607cd01258
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2026-08-13 17:22:54 +0530

    2213. Longest Substring of One Repeating Character

---
+++ leetcode solutions/2213. Longest Substring of One Repeating Character/Solution.java
@@ -0,0 +1,122 @@
+// 2213. Longest Substring of One Repeating Character
+
+/**
+ * Intuition
+ * we will solve this problem through segment tree
+ * we will first build segment tree
+ * it will be of size 4*n, where n is the length of the string
+ * segment tree will divide string in two parts and each part is again divided
+ * in two parts, till we have single chars left
+ * then we will traverse query and update chars of segment tree
+ * At last we will store max length of root node for each index in the res
+ * each node of segment tree will contain 5 information
+ * pre - length of prefix of repeating chars
+ * suf - length of suffix of repeating chars
+ * maxLen - length of max length of repeating chars
+ * leftChar - left char of the node
+ * rightChar - right char of the node
+ * merge method - it is taking max of left node and right node, longest
+ * repeating chars
+ * if right char of left node and left char of right node is same
+ * a new repeating string can be formed
+ * it is taking max of curr max and length of new repeating string
+ * Finally returning res
+ * Approach
+ * 
+ * Complexity
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+
+class Solution {
+    static class Node {
+        int pre = 0;
+        int suf = 0;
+        int maxLen = 0;
+        char leftChar = 0;
+        char rightChar = 0;
+
+        Node() {
+        }
+
+        Node(int pre, int suf, int maxLen, char leftChar, char rightChar) {
+            this.pre = pre;
+            this.suf = suf;
+            this.maxLen = maxLen;
+            this.leftChar = leftChar;
+            this.rightChar = rightChar;
+        }
+    }
+
+    int n;
+    Node[] segTree; // segment tree size 4*n
+
+    Node merge(Node L, Node R, int leftLen, int rightLen) {
+        Node res = new Node();
+
+        res.leftChar = L.leftChar;
+        res.rightChar = R.rightChar;
+
+        res.pre = L.pre;
+        if (L.pre == leftLen && L.rightChar == R.leftChar) {
+            res.pre = L.pre + R.pre;
+        }
+
+        res.suf = R.suf;
+        if (R.suf == rightLen && L.rightChar == R.leftChar) {
+            res.suf = R.suf + L.suf;
+        }
+
+        res.maxLen = Math.max(L.maxLen, R.maxLen);
+        if (L.rightChar == R.leftChar) {
+            res.maxLen = Math.max(res.maxLen, L.suf + R.pre);
+        }
+
+        return res;
+    }
+
+    void buildSegmentTree(int i, int l, int r, String s) {
+        if (l == r) {
+            segTree[i] = new Node(1, 1, 1, s.charAt(l), s.charAt(l));
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(2 * i + 1, l, mid, s);
+        buildSegmentTree(2 * i + 2, mid + 1, r, s);
+        segTree[i] = merge(segTree[2 * i + 1], segTree[2 * i + 2], mid - l + 1, r - mid);
+    }
+
+    void update(int i, int l, int r, int pos, char ch) {
+        if (l == r) { // l == r == pos
+            segTree[i] = new Node(1, 1, 1, ch, ch);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if (pos <= mid) {
+            update(2 * i + 1, l, mid, pos, ch);
+        } else {
+            update(2 * i + 2, mid + 1, r, pos, ch);
+        }
+        segTree[i] = merge(segTree[2 * i + 1], segTree[2 * i + 2], mid - l + 1, r - mid);
+    }
+
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        n = s.length();
+        segTree = new Node[4 * n]; // segment tree size 4*n
+
+        buildSegmentTree(0, 0, n - 1, s);
+
+        int k = queryIndices.length;
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            int pos = queryIndices[i];
+            char ch = queryCharacters.charAt(i);
+            update(0, 0, n - 1, pos, ch);
+
+            result[i] = segTree[0].maxLen; // root node covers entire string
+        }
+
+        return result;
+    }
+}
